5d9ceecb1078c417f26d88a685a2a20685e7a2ab
Jesse Jusoli <oliveira.jesse@gmail.com>
2017-08-27 00:24:56 +0000

    init

---
+++ release/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/workspace
+# Build directory: /home/ubuntu/workspace/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(external)
+subdirs(src)
+subdirs(tests)
---
+++ release/external/miniupnpc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/workspace/external/miniupnpc
+# Build directory: /home/ubuntu/workspace/build/release/external/miniupnpc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
